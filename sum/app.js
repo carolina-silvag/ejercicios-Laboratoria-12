5234f8092bef7907bf64e554a608d8cfd2d61eac
carolina-silvag <carolinasguzman@gmail.com>
2017-09-27 23:16:08 -0300

    algo de la primera parte del ejercicio1, funcion rango y suma

--- sum/app.js
+++ sum/app.js
@@ -1,1 +1,19 @@
+/*crear una funcion que me de un arreglo con todos los numeros de rango*/
 
+function rango(start, end){
+	var arreglo = []
+	for(var i = start; i<=end; i++){
+		arreglo[i-1]= i
+	}
+	return arreglo
+}
+
+function sum(arreglo){
+	var sumar = 0
+	for (var i= 0; i<arreglo.length; i++){
+		sumar += arreglo[i]
+	}
+	return sumar
+}
+
+console.log(sum(rango(1, 10)))
