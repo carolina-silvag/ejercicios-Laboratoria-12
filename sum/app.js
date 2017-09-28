33a8c2cc37ac126f0404d3837061c4764de67e1d
carolina-silvag <carolinasguzman@gmail.com>
2017-09-27 23:57:09 -0300

    ejercicio1 terminado con todo lo pedido

--- sum/app.js
+++ sum/app.js
@@ -1,12 +1,12 @@
 /*crear una funcion que me de un arreglo con todos los numeros de rango*/
 
-function rango(start, end){
+/*function rango(start, end){                //funcion no generica
 	var arreglo = []
 	for(var i = start; i<=end; i++){
 		arreglo[i-1]= i
 	}
 	return arreglo
-}
+}*/
 
 function sum(arreglo){
 	var sumar = 0
@@ -16,4 +16,24 @@ function sum(arreglo){
 	return sumar
 }
 
-console.log(sum(rango(1, 10)))
+function rango(start, end, step){               //funcion generica para positivos y negativos
+	var arreglo = []
+	if (step >= 0){
+		for(var i = start; i<=end; i+=step){
+			arreglo.push(i)
+		}
+	}else{
+		for(var i = start; i>= end; i+=step){
+			arreglo.push(i)
+		}
+	}
+	
+	return arreglo
+}
+
+//comprobar arreglos
+//console.log(rango(1, 10, 2))
+//console.log(rango(5, 2, -1))
+
+//console.log(sum(rango(1, 10, 2)))
+console.log(sum(rango(5, 2, -1)))
