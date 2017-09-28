f8bff84f11d7f225834774b924e9b295a3284ba3
carolina-silvag <carolinasguzman@gmail.com>
2017-09-28 00:30:06 -0300

    ejercicio2 resuelto

--- Scorekeeper/app.js
+++ Scorekeeper/app.js
@@ -1,1 +1,9 @@
+var score = {carolina : 0, fernanda : 0}        //crear objeto yo y mi amiga
 
+function sumarPuntaje(nombre, puntos){
+	score[nombre] += puntos
+}
+
+function totalPuntos(){
+	console.log(score)
+}
