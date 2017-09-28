75813f28870ab38d40727d139e95c8d86bf747f5
carolina-silvag <carolinasguzman@gmail.com>
2017-09-28 15:00:27 -0300

    ejercicio remover listo

--- RemoveDuplicates/app.js
+++ RemoveDuplicates/app.js
@@ -1,1 +1,10 @@
 
+function remove(array){
+	var arrayModificado=[]
+	for(var i = 0; i < array.length; i++){
+		if(array.indexOf(array[i]) != -1 && arrayModificado.indexOf(array[i]) == -1){
+			arrayModificado.push(array[i])
+		}
+	}
+	console.log(arrayModificado)
+}
