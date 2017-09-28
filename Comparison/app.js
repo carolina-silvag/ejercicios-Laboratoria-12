ae4f02ea4adaecf05b494938aa962157bc6ec763
carolina-silvag <carolinasguzman@gmail.com>
2017-09-28 00:56:47 -0300

    ejercicio de comparacion terminado

--- Comparison/app.js
+++ Comparison/app.js
@@ -1,1 +1,34 @@
 
+/*function comparar(array1, array2){                   //primera manera en como lo pense
+	if (array1.length == array2.length){
+		var comprobar= 0
+		for(var i= 0; i < array1.length; i++){
+			if (array1[i]!=array2[i]){
+				comprobar += 1
+			}
+		}
+		if (comprobar == 0){
+			return true
+		}else{
+			return false
+		}
+
+	}else{
+		return false
+	}
+	
+}*/
+
+function comparar(array1, array2){
+	var comprobar= 0
+	for(var i= 0; i < array1.length; i++){
+		if (array1[i]!=array2[i]){
+				comprobar += 1
+		}
+	}
+	if (comprobar == 0){
+		return true
+	}else{
+		return false
+	}
+}
